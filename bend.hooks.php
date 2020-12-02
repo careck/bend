933b3ac6300124580e36e8332d5c27405fc223e4
Carsten Eckelmann <carsten@2pisoftware.com>
2020-12-02 13:59:45 +1100

    fixed font-awesome stylesheet name

--- bend.hooks.php
+++ bend.hooks.php
@@ -7,5 +7,5 @@
  */
 function bend_core_web_after_get_bend(Web $w) {
 	$w->enqueueStyle(["name" => "bend.css", "uri" => "/modules/bend/assets/css/bend.css", "weight" => 500]);
-	$w->enqueueStyle(["name" => "font-awesome.css", "uri" => "/modules/bend/assets/css/font-awesome.min.css", "weight" => 501]);
+	$w->enqueueStyle(["name" => "font-awesome.min.css", "uri" => "/modules/bend/assets/css/font-awesome.min.css", "weight" => 501]);
 }
