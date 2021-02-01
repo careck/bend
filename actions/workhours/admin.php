11362c9235ef6543efe07eab2c1671e032da6533
Carsten Eckelmann <careck@gmail.com>
2021-02-01 19:54:18 +1100

    Removed enqueue script from admin.php

--- actions/workhours/admin.php
+++ actions/workhours/admin.php
@@ -3,7 +3,5 @@
 function admin_ALL(Web $w) {
     History::add("Workhours Admin");
     $w->ctx("workperiods",$w->Bend->getAllWorkPeriods());
-    $w->ctx("focusgroups",$w->Bend->getTopLevelWorkCategories());
-    $w->enqueueStyle(["uri" => "/modules/bend/assets/css/bend.css", "weight" => 500]);
-    
+    $w->ctx("focusgroups",$w->Bend->getTopLevelWorkCategories());   
 }
